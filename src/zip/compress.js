d4de7cee986d7f8e257b07000d453d47d6850802
dimtim1992 <dimtim7272807@gmail.com>
2022-12-08 21:13:38 +0300

    feat: filled zip directory

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
     // Write your code here 
+    const readStream = createReadStream(__dirname + '/files/fileToCompress.txt');
+    const writeStream = createWriteStream(__dirname + '/files/archive.gz');
+    const toGz = createGzip();
+    
+    readStream.pipe(toGz).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
     // Write your code here 
+    const readStream = createReadStream(__dirname + '/files/archive.gz');
+    const writeStream = createReadStream(__dirname + '/files/fileToCompress.txt');
+    const fromGz = createGzip();
+
+    readStream.pipe(fromGz).pipe(writeStream);
 };
 
 await decompress();
